4982b0c7228e1fca74d49cbed709be08c2cf5a15
lu-css <dossantossilvalucas963@gmail.com>
2023-11-24 08:54:19 -0300

    move dependency inversion project location

--- MyMeat.Application/Src/App/Meats/Entities/MeatValidation.cs
+++ MyMeat.Application/Src/App/Meats/Entities/MeatValidation.cs
@@ -6,8 +6,8 @@ public class MeatValidation : AbstractValidator<Meat>
 {
     public MeatValidation()
     {
-        RuleFor(x => x.Name).Length(5, 64);
-        RuleFor(x => x.Description).MaximumLength(256);
+        RuleFor(x => x.Name).MaximumLength(64);
+        RuleFor(x => x.Description).Length(5, 256);
         RuleFor(x => x.Price).GreaterThan(5);
         RuleFor(x => x.Validade).GreaterThan(DateTime.Now.AddMonths(1)).WithMessage("A validade mínima é de 1 Mẽs.");
     }
